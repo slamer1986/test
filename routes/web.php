d6e8d12edc65566360b86cfc58ccb5d0ff0634c0
Aleksandr Egorov <slamer86@gmail.com>
2018-01-12 16:29:23 +0300

    Первая версия с авторизацией

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::prefix('slamer')->group(function () {
+    Route::get('/', function () {
+        //print_r(DB::connection()->select('select * FROM sessions'));
+        $data = DB::connection()->table('sessions')->get();
+        echo $data;
+
+    });
+});
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
